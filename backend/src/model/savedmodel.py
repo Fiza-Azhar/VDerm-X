eb72915869d455cd9dfcfcb7563d77e922b3fe23
Maryam Siddique <125473579+MaryamSiddiq@users.noreply.github.com>
2024-11-16 12:00:00 -0800

    Exported the model in SavedModel format

--- backend/src/model/savedmodel.py
+++ backend/src/model/savedmodel.py
@@ -4,4 +4,4 @@ from tensorflow.keras.models import load_model
 model = load_model('final_model.keras')
 
 # Export the model in SavedModel format
-model.export('C:/Fypbackend/backend/src/model/saved_model')
+ model.export('C:/Fypbackend/backend/src/model/saved_model')
