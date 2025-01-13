31632b00212312545ef84db3e77b6fcf8ad34e90
Maryam Siddique <125473579+MaryamSiddiq@users.noreply.github.com>
2025-01-13 09:03:52 -0800

    Installing new packages

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,6 +6,8 @@ import { ImageModule } from './model/image.module';
 import { ImageModulee } from './image/image.module';
 import { VetModule } from './vet/vets.module';
 import * as dotenv from 'dotenv';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import path, { join } from 'path';
 dotenv.config();
 
 
@@ -13,6 +15,10 @@ dotenv.config();
   imports: [
     // Connect to MongoDB
     MongooseModule.forRoot(process.env.MONGODB_URI), // Replace with your MongoDB URL if different
+    ServeStaticModule.forRoot({
+      rootPath: path.join(__dirname, 'uploads'), // Path where files are stored (relative to the current directory)
+      serveRoot: '/uploads', // URL prefix for accessing the files
+    }),
     UserModule,
     //ImageModule,
     ImageModulee,
